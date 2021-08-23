11562cbda242dc4ced305a524afa752339a3bddb
Catherine Zwiercan <catherine.zwiercan@jbhunt.com>
2021-08-23 10:28:12 -0500

    init

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+
+@Component({
+  selector: 'fp-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+
+export class HeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
