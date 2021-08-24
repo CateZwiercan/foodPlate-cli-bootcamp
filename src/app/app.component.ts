55606ccca2256471c94b79c4e0841812610ead65
Catherine Zwiercan <catherine.zwiercan@jbhunt.com>
2021-08-24 13:25:07 -0500

    challenge 7,8,9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,6 +17,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit (){
     this.titleService.setTitle('Welcome to FoodPlate');
-    this.userService.getUser();
+    this.user = this.userService.getUser();
   }
 }
